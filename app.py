4556c6968c9bcf68ad176dfc3707db01e9bf75a7
MUHAMMAD ALI <muhammad.ali@dpschool.io>
2022-06-08 03:44:07 +0500

    Added dlib logic

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ wd = os.getcwd()
 os.chdir('dlib/')
 call('python setup.py install', shell=True)
 os.chdir(wd)
+call('pip uninstall dlib', shell=True)
+call('pip install dlib', shell=True)
 # call('wget https://files.pythonhosted.org/packages/05/57/e8a8caa3c89a27f80bc78da39c423e2553f482a3705adc619176a3a24b36/dlib-19.17.0.tar.gz', shell=True)
 
 # call('tar -xvzf dlib-19.17.0.tar.gz', shell=True)
