d992f26de559e4ef53b9a92584b2d58c89cc4c39
MUHAMMAD ALI <muhammad.ali@dpschool.io>
2022-06-07 22:30:24 +0500

    Added setuptools and wheels

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("Cartoon Image Generator")
 st.text("Either Upload or Provide URL of Your Image")
 
-call('pip install --upgrade pip', shell=True)
+call('pip install --upgrade pip setuptools wheel', shell=True)
 call('pip install opencv-python==4.5.3.56', shell=True)
 call('sudo apt-get -qq install subversion', shell=True)
 call('sudo apt-get -qq install libgl1-mesa-glx ', shell=True)
