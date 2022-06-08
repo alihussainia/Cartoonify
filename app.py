c532d4034aa69accaf61c309fef8b95a042b316e
MUHAMMAD ALI <muhammad.ali@dpschool.io>
2022-06-08 14:18:41 +0500

    back to dlib

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.text("Either Upload or Provide URL of Your Image")
 
 call('pip install --upgrade pip', shell=True)
 call('pip install cmake', shell=True)
-call('pip install https://files.pythonhosted.org/packages/0e/ce/f8a3cff33ac03a8219768f0694c5d703c8e037e6aba2e865f9bae22ed63c/dlib-19.8.1-cp36-cp36m-win_amd64.whl#sha256=794994fa2c54e7776659fddb148363a5556468a6d5d46be8dad311722d54bfcf', shell=True)
+call('pip3 install dlib', shell=True)
 call('gdown -q "1bjXhuTt0CNc5tqNN8ogcfB_F1V_RxnXM" -O models/', shell=True)
 # call('apt-get update', shell=True)
 # call('pip3 install cmake', shell=True)
